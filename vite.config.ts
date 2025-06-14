140672176306e4efb30ac89b13573750097e427d
kaushik9859 <bhowmickkaushik1@gmail.com>
2025-06-14 20:58:17 +0530

    finishing all changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,9 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    visualizer({
+      open: true, // Automatically open the report in the browser
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
